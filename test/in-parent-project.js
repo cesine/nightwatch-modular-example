c7ce5d4e4037d56990c9c92e4b3373e17c721408
cesine <cesine@yahoo.com>
2017-02-04 16:34:13 -0500

    using fork of nightwatch which supports tests in node_modules
    $ npm test
    
    > nightwatch-modular-example@1.0.0 test /Users/gchiodo/git/nightwatch-modular-example
    > nightwatch -c nightwatch.json
    
    Starting selenium server in parallel mode... started - PID:  19279
    
    Started child process for: test/in-parent-project
    Started child process for: test/in-component-a
    Started child process for: test/in-component-b
    \n
    [In Component A] Test Suite
    ===============================
    \n
    [In Parent Project] Test Suite
    ==================================
    
    Results for:  should have expected url
    
    Results for:  should have a page title
    using config Config {
      something: 'from sample a default config',
      repo: { owner: 'tatsuyafw/', slug: 'gulp-nightwatch' } }
    using config Config { test: { locale: 'es-mx' } }
    \n
    [In Component B] Test Suite
    ===============================
    
    Results for:  should support unicode urls
    using config Config {
      something: 'from sample b default config',
      wikipedia: { article: 'დედა_ენა', locale: 'ka' } }
     ✔ Testing if the URL equals "https://github.com/tatsuyafw//".
    
    OK. 1 assertions passed. (2.855s)
    
      >> test/in-component-a finished: 0
    
     ✔ Testing if the URL equals "https://ka.m.wikipedia.org/wiki/%E1%83%93%E1%83%94%E1%83%93%E1%83%90_%E1%83%94%E1%83%9C%E1%83%90".
    
    OK. 1 assertions passed. (4.485s)
    
      >> test/in-component-b finished: 0
    
     ✔ Testing if the page title equals "Condiciones de uso -- craigslist".
    
    OK. 1 assertions passed. (5.719s)
    
      >> test/in-parent-project finished: 0
    
    INFO Selenium process finished.

--- test/in-parent-project.js
+++ test/in-parent-project.js
@@ -2,6 +2,7 @@ var config = require('config');
 
 module.exports = {
   'should have a page title': function(client) {
+    console.log('using config', config);
     client
       .url('https://www.craigslist.org/about/terms.of.use.' + config.test.locale)
       .assert.title('Condiciones de uso -- craigslist')
